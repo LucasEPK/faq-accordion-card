1dc17b31c49d3b64170307aef01d1bb05599df14
LucasEPK <lucasmoyano11@outlook.it>
2023-06-03 23:18:30 -0300

    Collapsibles now collapse if another is selected

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ for(i=0; i<que.length; i++){
       this.classList.toggle("active");
 
       /* Toggle between hiding and showing the active panel */
-      let panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
+      let panel = this.nextElementSibling; //nextElementSibling takes the next element in the HTML
+      let other_panel
+      if (panel.style.display === "block") {//hides panel if clicked
         panel.style.display ="none";
-      } else {
+      } else { //shows panel if clicked but closes the other panels that are opened
         panel.style.display = "block";
+
+        //closes all the other panels
+        for(j=0; j<que.length; j++){
+          other_panel = que[j].nextElementSibling;
+          if(this != que[j] && other_panel.style.display === "block"){
+            que[j].classList.toggle("active");
+            other_panel.style.display = "none";
+          }
+        }
       }
     });
 }
