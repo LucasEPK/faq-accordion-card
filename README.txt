add3a2d623a898d14cfe7e03f5277305b742f1ba
LucasEPK <lucasmoyano11@outlook.it>
2023-06-16 15:50:00 -0300

    Finished desktop version

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 Frontend mentor challenge that I still haven't finished
+Works for desktop, now I gotta do mobile.
--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //this shows the text that is hidden otherwise, in an accordion fashion
-let que = document.getElementsByClassName("question_txt");
+let questions = document.getElementsByClassName("question_txt");
 let i;
 
-for(i=0; i<que.length; i++){
-    que[i].addEventListener("click", function(){
-      /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
+for(i=0; i<questions.length; i++){
+    questions[i].addEventListener("click", function(){
+      /* Toggle between adding and removing the "active" class, to highlight the button that controls the answer */
       this.classList.toggle("active");
 
-      /* Toggle between hiding and showing the active panel */
-      let panel = this.nextElementSibling; //nextElementSibling takes the next element in the HTML
-      let other_panel
-      if (panel.style.display === "block") {//hides panel if clicked
-        panel.style.display ="none";
-      } else { //shows panel if clicked but closes the other panels that are opened
-        panel.style.display = "block";
+      /* Toggle between hiding and showing the active answer */
+      let answer = this.nextElementSibling; //nextElementSibling takes the next element in the HTML
+      let other_answer;
+      if (answer.style.display === "block") {//hides answer if clicked
+        answer.style.display ="none";
+      } else { //shows answer if clicked but closes the other answers that are opened
+        answer.style.display = "block";
 
-        //closes all the other panels
-        for(j=0; j<que.length; j++){
-          other_panel = que[j].nextElementSibling;
-          if(this != que[j] && other_panel.style.display === "block"){
-            que[j].classList.toggle("active");
-            other_panel.style.display = "none";
+        //closes all the other answers
+        for(j=0; j<questions.length; j++){
+          other_answer = questions[j].nextElementSibling;
+          if(this != questions[j] && other_answer.style.display === "block"){
+            questions[j].classList.toggle("active");
+            other_answer.style.display = "none";
           }
         }
       }
